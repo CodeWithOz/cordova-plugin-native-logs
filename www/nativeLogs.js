9f11ceeb5c5152ce1da7b5b103c876577e47ce62
Uche Ozoemena <ucheozoemena@gmail.com>
2020-12-17 10:47:50 +0100

    Create js method for getting pushwoosh logs

--- www/nativeLogs.js
+++ www/nativeLogs.js
@@ -5,6 +5,10 @@ module.exports = {
 
 	getLog:function(_nbLines,_bCopyToClipboard,successCB,failureCB){
 		cordova.exec(successCB, failureCB, this.pluginName,"getLog", [_nbLines,_bCopyToClipboard]);
+	},
+
+	getPushwooshLog:function(_nbLines,_bCopyToClipboard,successCB,failureCB){
+		cordova.exec(successCB, failureCB, this.pluginName,"getPushwooshLog", [_nbLines,_bCopyToClipboard]);
 	}
 };
 
